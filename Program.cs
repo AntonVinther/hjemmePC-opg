90b0b40ca7ab1c7f8e5cd000d12e02c4e6cf3737
Anton <anto3673@mail.hansenberg.dk>
2024-01-25 22:28:32 +0100

    Prime...kinda

--- Program.cs
+++ Program.cs
@@ -242,5 +242,35 @@ sumsqrd = sumsqrd * sumsqrd;
 int diff = sumsqrd - squares;
 Console.WriteLine(diff);
 
+using System.Globalization;
 */
+
+using System;
+
+public class PrimeChecker
+{
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void Main()
+    {
+        for (int i = 2; i < 1e9; i++)
+        {
+            int number = i;
+            Console.WriteLine($"{number} is {(IsPrime(number) ? "prime" : "not prime")}.");
+        }
+    }
+}
+
 
