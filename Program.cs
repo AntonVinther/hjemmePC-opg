56709a57875752bcdd05b4374e6c744bce1c4155
AntonVinther <143699947+AntonVinther@users.noreply.github.com>
2024-01-26 14:27:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -243,7 +243,7 @@ int diff = sumsqrd - squares;
 Console.WriteLine(diff);
 
 using System.Globalization;
-*/
+
 using System;
 
 public class PrimeChecker
@@ -281,5 +281,30 @@ public class PrimeChecker
 
     }
 }
+*/
+using System;
 
+public class pythagoreanTriple
+{
+    public static void Main()
+    {
+    for (int a = 1; a < 1000; a++)
+    {
+        for (int b = 1; b < 1000; b++)
+        {
+            for (int c = 1; c < 1000; c++)
+            {
+                if (a * a + b * b == c * c)
+                {
+                    if (a + b + c == 1000)
+                    {
+                        Console.WriteLine(a * b * c);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+}
 
